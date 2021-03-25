bf1ec2147b15e6c2ce74b97b8211c4a33321adb8
Cagtay BAYRAK <cagtay.bayrak@digitalplanet.com.tr>
2021-03-25 22:15:31 +0300

    sadsadasdasdasdasd

--- CRUDWinForm-StoredProcedures/FomDetails/Form1.Designer.cs
+++ CRUDWinForm-StoredProcedures/FomDetails/Form1.Designer.cs
@@ -28,30 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegistration = new System.Windows.Forms.Button();
+            this.btnInvoice = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Red;
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnInvoice);
+            this.panel1.Controls.Add(this.btnRegistration);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 204);
+            this.panel1.Size = new System.Drawing.Size(800, 121);
             this.panel1.TabIndex = 0;
             // 
-            // button1
+            // btnRegistration
             // 
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(24, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(271, 66);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRegistration.BackColor = System.Drawing.Color.Red;
+            this.btnRegistration.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistration.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRegistration.ForeColor = System.Drawing.Color.White;
+            this.btnRegistration.Image = ((System.Drawing.Image)(resources.GetObject("btnRegistration.Image")));
+            this.btnRegistration.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistration.Location = new System.Drawing.Point(12, 12);
+            this.btnRegistration.Name = "btnRegistration";
+            this.btnRegistration.Size = new System.Drawing.Size(188, 82);
+            this.btnRegistration.TabIndex = 0;
+            this.btnRegistration.Text = "Registration";
+            this.btnRegistration.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRegistration.UseVisualStyleBackColor = false;
+            // 
+            // btnInvoice
+            // 
+            this.btnInvoice.BackColor = System.Drawing.Color.Red;
+            this.btnInvoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInvoice.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnInvoice.ForeColor = System.Drawing.Color.White;
+            this.btnInvoice.Image = ((System.Drawing.Image)(resources.GetObject("btnInvoice.Image")));
+            this.btnInvoice.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInvoice.Location = new System.Drawing.Point(215, 12);
+            this.btnInvoice.Name = "btnInvoice";
+            this.btnInvoice.Size = new System.Drawing.Size(135, 82);
+            this.btnInvoice.TabIndex = 0;
+            this.btnInvoice.Text = "Invoice";
+            this.btnInvoice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnInvoice.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -73,6 +98,9 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnInvoice;
+        private System.Windows.Forms.Button btnRegistration;
     }
 }
 
