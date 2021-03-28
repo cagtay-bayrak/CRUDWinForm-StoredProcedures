fb6c4d395ad5225972b3907cdbe205b959278931
Cagtay BAYRAK <cagtay.bayrak@digitalplanet.com.tr>
2021-03-28 14:17:46 +0300

    update

---
+++ CRUDWinForm-StoredProcedures/FomDetails/RegistrationForms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FomDetails
+{
+    public partial class RegistrationForms : Form
+    {
+        public RegistrationForms()
+        {
+            InitializeComponent();
+        }
+    }
+}
