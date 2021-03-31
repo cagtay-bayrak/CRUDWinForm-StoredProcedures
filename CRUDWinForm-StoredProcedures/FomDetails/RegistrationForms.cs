65beb64df810748f45f44ab5534b7576cd1dfeb5
Cagtay BAYRAK <cagtay.bayrak@digitalplanet.com.tr>
2021-03-31 14:41:26 +0300

    save

--- CRUDWinForm-StoredProcedures/FomDetails/RegistrationForms.cs
+++ CRUDWinForm-StoredProcedures/FomDetails/RegistrationForms.cs
@@ -15,6 +15,7 @@ namespace FomDetails
 
         string path = @"Data Source=.;Initial Catalog=CRUD_SP_DB;Integrated Security=True";
         SqlConnection con;
+        SqlCommand cmd;
 
 
         public RegistrationForms()
@@ -30,7 +31,40 @@ namespace FomDetails
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if (txtfname.Text == "" || txtfname.Text == "" || txtDesign.Text == "" || txteMail.Text == "" || txtID.Text == "")
+            {
+                MessageBox.Show("selected data");
+            }
 
+            else
+            {
+
+                try
+                {
+
+                    string Gender;
+                    if (rdoMale.Checked)
+                    {
+                        Gender = "Male";
+                    }
+                    else
+                    {
+                        Gender = "Female";
+                    }
+                    con.Open();
+                    cmd = new SqlCommand("insert into Employee (Name,Fname,Design,eMail,Emp_ID,Gender,Address) values ('" + txtName.Text + "','" + txtfname.Text + "','" + txtDesign.Text + "''" + txteMail.Text + "''" + txtID.Text + "''" + Gender + "''" + txtAddress.Text + "')", con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("You Data has saved");
+                }
+                catch (Exception ex)
+                {
+
+                    //MessageBox.Show(ex.Message);
+                }
+
+
+            }
         }
 
         private void bunUpdate_Click(object sender, EventArgs e)
--- CRUDWinForm-StoredProcedures/FomDetails/readme.txt
+++ CRUDWinForm-StoredProcedures/FomDetails/readme.txt
@@ -1,2 +1,38 @@
 ﻿create db CRUD_SP_DB;
 
+create table Employee
+(
+ID int primary key identity(1,1),
+Name nvarchar(50),
+Fname nvarchar(50),
+Design nvarchar(50),
+eMail nvarchar(50),
+Emp_ID nvarchar(50),
+Gender nvarchar(50),
+Address nvarchar(50),
+)
+
+
+USE [CRUD_SP_DB]
+GO
+
+INSERT INTO [dbo].[Employee]
+           ([Name]
+           ,[Fname]
+           ,[Design]
+           ,[eMail]
+           ,[Emp_ID]
+           ,[Gender]
+           ,[Address])
+     VALUES
+           ('Cagtay',
+            'Bayrak',
+            'Test',
+         'bayrak.cagtay@gmail.com',
+          '28',
+         'Male',
+		 'bayrampaşa')
+ 
+
+
+
