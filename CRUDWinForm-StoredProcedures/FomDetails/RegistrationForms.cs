03ebee0d209ca9e1923e2145c01f7cd32bde38ce
Cagtay BAYRAK <cagtay.bayrak@digitalplanet.com.tr>
2021-03-31 13:58:19 +0300

    efsefs

--- CRUDWinForm-StoredProcedures/FomDetails/RegistrationForms.cs
+++ CRUDWinForm-StoredProcedures/FomDetails/RegistrationForms.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -11,12 +12,15 @@ namespace FomDetails
     public partial class RegistrationForms : Form
     {
 
+
         string path = @"Data Source=.;Initial Catalog=CRUD_SP_DB;Integrated Security=True";
-        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=CRUD_SP_DB;Integrated Security=True");
+        SqlConnection con;
+
 
         public RegistrationForms()
         {
             InitializeComponent();
+            con = new SqlConnection(path);
         }
 
         private void RegistrationForms_Load(object sender, EventArgs e)
